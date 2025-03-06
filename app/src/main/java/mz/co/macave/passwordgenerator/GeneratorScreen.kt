cedeb9cd099b737a53ea8d307a40479800b53f6f
António Macave <antonio.macave@gmail.com>
2025-03-06 10:24:56 +0200

    Added: Password Length Range Slider

--- app/src/main/java/mz/co/macave/passwordgenerator/GeneratorScreen.kt
+++ app/src/main/java/mz/co/macave/passwordgenerator/GeneratorScreen.kt
@@ -1,19 +1,20 @@
 package mz.co.macave.passwordgenerator
 
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Checkbox
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.Slider
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableFloatStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
@@ -26,9 +27,9 @@ import androidx.compose.ui.unit.dp
 
 @Composable
 fun TextInput() {
-    
+
     var text by remember { mutableStateOf("") }
-    
+
     Row(
         verticalAlignment = Alignment.CenterVertically
     ) {
@@ -60,9 +61,33 @@ fun TextInput() {
                     id = android.R.string.copy)
                 )
         }
-        
     }
-    
+}
+
+
+@Composable
+fun PasswordLengthRange() {
+    var currentValue by remember { mutableFloatStateOf(10f) }
+    Row(
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Slider(
+            modifier = Modifier
+                .padding(24.dp)
+                .fillMaxWidth()
+                .weight(0.85f),
+            value = currentValue,
+            onValueChange = { value ->
+                currentValue = value
+            },
+            valueRange = 10f..100f,
+        )
+        
+        Text(
+            modifier = Modifier.weight(0.15f),
+            text = currentValue.toInt().toString()
+        )
+    }
 }
 
 
@@ -75,19 +100,19 @@ fun OptionsToInclude() {
     var includeSpecialCharacters by remember { mutableStateOf(false) }
 
 
-    CheckBoxOption(text = "Incluir letras maiúsculas", checked = includeCapitalLetters) {
+    CheckBoxOption(text = stringResource(id = R.string.include_capital_letters), checked = includeCapitalLetters) {
         includeCapitalLetters = !includeCapitalLetters
     }
 
-    CheckBoxOption(text = "Incluir letras minúsculas", checked = includeNonCapitalLetters) {
+    CheckBoxOption(text = stringResource(id = R.string.include_noncapital_letters), checked = includeNonCapitalLetters) {
         includeNonCapitalLetters = !includeNonCapitalLetters
     }
 
-    CheckBoxOption(text = "Incluir números", checked = includeNumbers) {
+    CheckBoxOption(text = stringResource(id = R.string.include_numbers), checked = includeNumbers) {
         includeNumbers = !includeNumbers
     }
 
-    CheckBoxOption(text = "Incluir caracteres especiais", checked = includeSpecialCharacters) {
+    CheckBoxOption(text = stringResource(id = R.string.include_special_chars), checked = includeSpecialCharacters) {
         includeSpecialCharacters = !includeSpecialCharacters
     }
 }
@@ -95,6 +120,9 @@ fun OptionsToInclude() {
 @Composable
 fun CheckBoxOption(text: String, checked: Boolean, onCheckedChanged: () -> Unit) {
     Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .clickable { onCheckedChanged() },
         verticalAlignment = Alignment.CenterVertically
     ) {
         Checkbox(checked = checked, onCheckedChange = { onCheckedChanged() } )
@@ -107,6 +135,7 @@ fun CheckBoxOption(text: String, checked: Boolean, onCheckedChanged: () -> Unit)
 fun Previews() {
     Column {
         TextInput()
+        PasswordLengthRange()
         OptionsToInclude()
     }
 }
