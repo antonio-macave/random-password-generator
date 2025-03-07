6e261de8be85106fb8bef373c521c25629efc7e4
António Macave <antonio.macave@gmail.com>
2025-03-07 09:48:32 +0200

    Added: UI States to the ViewModel

--- app/src/main/java/mz/co/macave/passwordgenerator/viewmodel/MainActivityViewModel.kt
+++ app/src/main/java/mz/co/macave/passwordgenerator/viewmodel/MainActivityViewModel.kt
@@ -1,10 +1,32 @@
 package mz.co.macave.passwordgenerator.viewmodel
 
 import androidx.lifecycle.ViewModel
-import java.util.Random
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
 import kotlin.random.Random
 
 class MainActivityViewModel : ViewModel() {
+
+
+    private val _generatedPassword: MutableStateFlow<String> = MutableStateFlow("")
+    val password: StateFlow<String> get() = _generatedPassword.asStateFlow()
+
+    private val _sliderValue: MutableStateFlow<Int> = MutableStateFlow(0)
+    val sliderValue: StateFlow<Int> get() = _sliderValue.asStateFlow()
+
+    private val _includeCapitalLetter: MutableStateFlow<Boolean> = MutableStateFlow(false)
+    val includeCapitalLetters: StateFlow<Boolean> get() = _includeCapitalLetter.asStateFlow()
+
+    private val _includeNonCapitalLetter: MutableStateFlow<Boolean> = MutableStateFlow(false)
+    val includeNonCapitalLetters: StateFlow<Boolean> get() = _includeNonCapitalLetter.asStateFlow()
+
+    private val _includeNumbers: MutableStateFlow<Boolean> = MutableStateFlow(false)
+    val includeNumbers: StateFlow<Boolean> get() = _includeNumbers.asStateFlow()
+
+    private val _includeSpecialChars: MutableStateFlow<Boolean> = MutableStateFlow(false)
+    val includeSpecialChars: StateFlow<Boolean> get() = _includeSpecialChars.asStateFlow()
+
 
     private fun getDefinedChars(
         includeCapitalLetters: Boolean,
