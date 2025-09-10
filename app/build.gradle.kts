e53fd64a747f7fb629a9f3a0d04c4aec87fd9525
António Macave <antonio.macave@gmail.com>
2025-09-10 16:06:11 +0200

    Upgrading compile and Target SDKs

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "mz.co.macave.passwordgenerator"
-    compileSdk = 35
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "mz.co.macave.passwordgenerator"
         minSdk = 24
-        targetSdk = 34
+        targetSdk = 36
         versionCode = 1
         versionName = "1.0"
 
